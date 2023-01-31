e5a2f4bb060074bc0d9940f3bde11ab1a82cda1f
jonathanalbertorosalessanchez <jhonathan.rsanchez09@gmail.com>
2023-01-31 14:00:40 -0600

    Primer commint

---
+++ src/main/java/com/proyect/ProyectJWT/ProyectJwtApplication.java
@@ -0,0 +1,13 @@
+package com.proyect.ProyectJWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyect/ProyectJWT/ProyectJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyect.ProyectJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
