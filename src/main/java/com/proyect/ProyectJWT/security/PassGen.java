4c85db4bdfb61fb7c1dca2cb53c7c5099fb9142e
frank <francisco.graya26@gmail.com>
2023-02-01 11:21:48 -0600

    se modifico base y se agrego usuario admi

--- src/main/java/com/proyect/ProyectJWT/security/PassGen.java
+++ src/main/java/com/proyect/ProyectJWT/security/PassGen.java
@@ -8,9 +8,13 @@ public class PassGen {
 
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
         String pass1 = "admin";
+        String pass2 = "jonantan";
         System.out.println(encoder.encode(pass1));
+        System.out.println(encoder.encode(pass2));
 
     }
 }
 //"admin"
 //$2a$10$rJ5.HsQcmsP3BlpoYLib3O8ZyZ8eyjFEoT3tjc4DV.lKPeappxf2K
+//jonatan
+//$2a$10$8szqqZ/6giezBbwupOdhE.jZ8o1FU5TiWCBiKHjzA89maCpt6ndWO
