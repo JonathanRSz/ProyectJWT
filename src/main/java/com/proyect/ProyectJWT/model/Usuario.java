85dff0bceab8d6466eef33c2be8fda6b7854a010
frank <francisco.graya26@gmail.com>
2023-02-01 10:36:28 -0600

    se modifico modelo en sus persistencia

--- src/main/java/com/proyect/ProyectJWT/model/Usuario.java
+++ src/main/java/com/proyect/ProyectJWT/model/Usuario.java
@@ -5,79 +5,18 @@ import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "usuarios")
 public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="IDUSUARIO")
-    private int idUsuario;
-    @Column(name="NOMBRE")
+    @Column(name="user_id")
+    private Long id;
+    @Column(name="nombre")
     private String nombre;
-    @Column(name="APELLIDO")
+    @Column(name="apellido")
     private String apellido;
-    @Column(name="EMAIL")
+    @Column(name="email")
     private String email;
-    @Column(name="PASS")
+    @Column(name="password")
     private String password;
-
-    public Usuario() {
-    }
-
-    public Usuario(int idUsuario, String nombre, String apellido, String email, String password) {
-        this.idUsuario = idUsuario;
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.email = email;
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "Usuario{" +
-                "idUsuario=" + idUsuario +
-                ", nombre='" + nombre + '\'' +
-                ", apellido='" + apellido + '\'' +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
-
-    public int getIdUsuario() {
-        return idUsuario;
-    }
-
-    public void setIdUsuario(int idUsuario) {
-        this.idUsuario = idUsuario;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
